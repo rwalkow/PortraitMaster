67341a0cb663c564472e7246810dd281cde58829
Rafal Walkow <r.walkow@po.edu.pl>
2022-09-13 21:49:12 +0200

    Add regular expression pattern for form checking

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -4,15 +4,26 @@ const Photo = require('../models/photo.model');
 
 exports.add = async (req, res) => {
   try {
-    const { title, author, email } = req.fields;
     const file = req.files.file;
+    const { title, author, email } = req.fields;
 
     if (title && author && email && file) {
       // if fields are not empty...
 
+      const authorPattern = new RegExp(/(<\s*(strong|em)*>(([A-z]|\s)*)<\s*\/\s*(strong|em)>)|(([A-z]|\s|\.)*)/,'g');
+      const emailPattern = new RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,'g');
+      const isAuthorValid = authorPattern.test(author);
+      const isEmailValid = emailPattern.test(email);
+      if (!isAuthorValid) {
+        throw new Error('Author is not valid');
+      } else if (!isEmailValid) {
+        throw new Error('Email is not valid');
+      }
+
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
       const fileExt = fileName.split('.').slice(-1)[0];
-      if ((fileExt[1] === '.jpg' || '.png' || '.gif') && title.length <= 25) {
+      if (
+        (fileExt[1] === '.jpg' || '.png' || '.gif') && title.length <= 50 && author.length <= 50) {
         const newPhoto = new Photo({
           title,
           author,
@@ -20,6 +31,7 @@ exports.add = async (req, res) => {
           src: fileName,
           votes: 0,
         });
+
         await newPhoto.save(); // ...save new photo in DB
         res.json(newPhoto);
       } else {
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const connectToDB = require('./db');
 
 // start express server
 const app = express();
+
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
 });
@@ -17,14 +18,18 @@ connectToDB();
 
 // add middleware
 app.use(cors());
-app.use(formidable({ uploadDir: './public/uploads/' }, [{
-  event: 'fileBegin', // on every file upload...
-    action: (req, res, next, name, file) => {
-      const fileName = uniqid() + '.' + file.name.split('.')[1];
-      file.path = __dirname + '/public/uploads/photo_' + fileName; // ...move the file to public/uploads with unique name
-    }
-  },
-]));
+
+app.use(
+  formidable({ uploadDir: './public/uploads/' }, [
+    {
+      event: 'fileBegin', // on every file upload...
+      action: (req, res, next, name, file) => {
+        const fileName = uniqid() + '.' + file.name.split('.')[1];
+        file.path = __dirname + '/public/uploads/photo_' + fileName; // ...move the file to public/uploads with unique name
+      },
+    },
+  ])
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
