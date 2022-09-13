a9c475707696c77c7d8c1401beff05dad64b4a94
Rafal Walkow <r.walkow@po.edu.pl>
2022-09-13 22:23:41 +0200

    Add checking tittle length

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -3,54 +3,58 @@ const Photo = require('../models/photo.model');
 /****** SUBMIT PHOTO ********/
 
 exports.add = async (req, res) => {
-
   try {
     const { title, author, email } = req.fields;
     const file = req.files.file;
 
-    if(title && author && email && file) { // if fields are not empty...
+    if (title && author && email && file) {
+      // if fields are not empty...
 
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
-      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
-      await newPhoto.save(); // ...save new photo in DB
-      res.json(newPhoto);
-
+      const fileExt = fileName.split('.').slice(-1)[0];
+      if ((fileExt[1] === '.jpg' || '.png' || '.gif') && title.length <= 25) {
+        const newPhoto = new Photo({
+          title,
+          author,
+          email,
+          src: fileName,
+          votes: 0,
+        });
+        await newPhoto.save(); // ...save new photo in DB
+        res.json(newPhoto);
+      } else {
+        throw new Error('Wrong file type!');
+      }
     } else {
       throw new Error('Wrong input!');
     }
-
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
-
 };
 
 /****** LOAD ALL PHOTOS ********/
 
 exports.loadAll = async (req, res) => {
-
   try {
     res.json(await Photo.find());
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
-
 };
 
 /****** VOTE FOR PHOTO ********/
 
 exports.vote = async (req, res) => {
-
   try {
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
-    if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
+    if (!photoToUpdate) res.status(404).json({ message: 'Not found' });
     else {
       photoToUpdate.votes++;
       photoToUpdate.save();
       res.send({ message: 'OK' });
     }
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
-
 };
